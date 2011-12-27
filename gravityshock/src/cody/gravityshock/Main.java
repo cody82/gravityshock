2ba1621bb281131ef9be5316834194cc5c2a73b9
cody <cody@localhost>
2011-12-27 21:45:21 +0100

    switch to opengl 2.0

--- gravityshock/src/cody/gravityshock/Main.java
+++ gravityshock/src/cody/gravityshock/Main.java
@@ -2,6 +2,7 @@ package cody.gravityshock;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.IntBuffer;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
@@ -14,6 +15,8 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.graphics.glutils.FrameBuffer;
+import com.badlogic.gdx.graphics.Pixmap;
 
 import com.gemserk.util.ScreenshotSaver;
 
@@ -35,6 +38,12 @@ public class Main implements ApplicationListener {
     boolean record = false;
     int frame = 1;
     
+    FrameBuffer framebuffer;
+    FrameBuffer framebuffer2;
+
+	GL10 gl10;
+	GL20 gl20;
+	
     void createPlayer(){
     	Spaceship oldplayer = player;
 		player = new Spaceship();
@@ -59,17 +68,35 @@ public class Main implements ApplicationListener {
 		font.setColor(Color.WHITE);
 		//texture = new Texture(Gdx.files.internal("badlogic.jpg"))
 		spriteBatch = new SpriteBatch();
-                
+
+		GL10 gl10 = Gdx.graphics.getGL10();
+		GL20 gl20 = Gdx.graphics.getGL20();
 		
+		if(Gdx.graphics.isGL20Available()) {
+			framebuffer = new FrameBuffer(Pixmap.Format.RGB888, 128, 128, false);
+			framebuffer2 = new FrameBuffer(Pixmap.Format.RGB888, 512, 512, false);
+		}
+
 		//var musicfile = Gdx.files.internal("data/2ND_PM.ogg")
 		//var music = Gdx.audio.newMusic(musicfile)
 		//music.play
     
-		cam = new OrthographicCamera(640, 480);
+		cam = new OrthographicCamera(256, 256);
 		
 		nextLevel();
 	}
 
+	void clear() {
+		if(gl10 != null) {
+			gl10.glClear(GL10.GL_COLOR_BUFFER_BIT);
+			gl10.glEnable(GL10.GL_BLEND);
+		}
+		else if(gl20 != null){
+			gl20.glClear(GL20.GL_COLOR_BUFFER_BIT);
+			gl20.glEnable(GL20.GL_BLEND);
+		}
+		
+	}
 	@Override
 	public void render () {
 
@@ -78,25 +105,48 @@ public class Main implements ApplicationListener {
 			// 30 FPS
 			t = 1f/30f;
 		}
+
+		gl10 = Gdx.graphics.getGL10();
+		gl20 = Gdx.graphics.getGL20();
+
+		cam.update();
 		
-		GL10 gl10 = Gdx.graphics.getGL10();
-		if(gl10 != null) {
-			gl10.glClear(GL10.GL_COLOR_BUFFER_BIT);
-			gl10.glEnable(GL10.GL_BLEND);
+		if(gl20 != null) {
+			framebuffer.begin();
+			clear();
+			world.render(cam);
+			framebuffer.end();
+			framebuffer2.begin();
+			clear();
+			world.render(cam);
+			framebuffer2.end();
 		}
 		else {
-
-			GL20 gl20 = Gdx.graphics.getGL20();
-			gl20.glClear(GL20.GL_COLOR_BUFFER_BIT);
-			gl20.glEnable(GL20.GL_BLEND);
+			clear();
+			world.render(cam);
 		}
-		cam.update();
-		world.render(cam);
-
-		
-		world.tick(t);
-		map.tick(t);
     
+
+		if(gl20 != null) {
+			//clear();
+			Texture texture = framebuffer.getColorBufferTexture();
+			Texture texture2 = framebuffer2.getColorBufferTexture();
+			
+
+			SpriteBatch spriteBatch2 = new SpriteBatch();
+			spriteBatch2.setBlendFunction(GL20.GL_ONE, GL20.GL_ONE);
+			spriteBatch2.begin();
+			spriteBatch2.setProjectionMatrix(spriteBatch2.getProjectionMatrix().setToOrtho2D(0, 1, 1, -1));
+			spriteBatch2.disableBlending();
+			spriteBatch2.setColor(0.2f, 0.2f, 0.2f, 1);
+			spriteBatch2.draw(texture, 0, 0, 1, 1);
+			spriteBatch2.enableBlending();
+			spriteBatch2.setColor(1, 1, 1, 1);
+			spriteBatch2.draw(texture2, 0, 0, 1, 1);
+			spriteBatch2.end();
+			spriteBatch2.dispose();
+		}
+
 		if(player.health <= 0) {
 			if(lifes <= 1) {
 				spriteBatch.begin();
@@ -139,12 +189,24 @@ public class Main implements ApplicationListener {
 			e.printStackTrace();
 		}
 		}
+		
+		world.tick(t);
+		map.tick(t);
 	}
 
 	@Override
 	public void resize (int width, int height) {
-	    cam.setToOrtho(false, width, height);
-		spriteBatch.getProjectionMatrix().setToOrtho2D(0, 0, width, height);
+		//if(gl20 == null) {
+			cam.setToOrtho(false, width, height);
+			spriteBatch.getProjectionMatrix().setToOrtho2D(0, 0, width, height);
+		//}
+
+			if(Gdx.graphics.isGL20Available()) {
+				framebuffer.dispose();
+				framebuffer2.dispose();
+				framebuffer = new FrameBuffer(Pixmap.Format.RGB888, width / 4, height / 4, false);
+				framebuffer2 = new FrameBuffer(Pixmap.Format.RGB888, width, height, false);
+			}
 	}
 
 	@Override
