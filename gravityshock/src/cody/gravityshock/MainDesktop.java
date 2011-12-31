2fca2edf9442f47a303962a4cc07cb0f0e4c503e
cody <cody@localhost>
2011-12-31 21:33:46 +0100

    accept commandline parameter "--numplayers X"

--- gravityshock/src/cody/gravityshock/MainDesktop.java
+++ gravityshock/src/cody/gravityshock/MainDesktop.java
@@ -4,6 +4,14 @@ import com.badlogic.gdx.backends.jogl.JoglApplication;
 
 public class MainDesktop {
 	public static void main (String[] argv) {
-		new JoglApplication(new Main(), "GravityShock", 800, 450, false);
+		Main main = new Main();
+
+		for(int i = 0;i<argv.length;++i) {
+			if(argv[i].equals("--numplayers")) {
+				main.numplayers = Integer.parseInt(argv[i+1]);
+			}
+		}
+		
+		new JoglApplication(main, "GravityShock", 800, 450, false);
 	}
 }
