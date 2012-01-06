97d74652c83347f6233635b66f1864f8ba64bac7
cody <cody@localhost>
2012-01-06 23:28:05 +0100

    fix

--- gravityshock/src/cody/gravityshock/MainMenu.java
+++ gravityshock/src/cody/gravityshock/MainMenu.java
@@ -59,13 +59,11 @@ public class MainMenu implements Screen {
 	@Override
 	public void dispose() {
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void hide() {
-		// TODO Auto-generated method stub
-		
+		Gdx.input.setInputProcessor(null);
 	}
 
 	@Override
