631f3f63c710a08ec5441b0d5fa14902fbd6f240
cody <cody@localhost>
2012-01-11 20:10:15 +0100

    android data storage

--- gravityshock-android/src/cody/gravityshock/MainAndroid.java
+++ gravityshock-android/src/cody/gravityshock/MainAndroid.java
@@ -11,7 +11,7 @@ public class MainAndroid extends AndroidApplication {
 	@Override
 	public void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		initialize(maingame = new MainGame(new AndroidData()), true);
+		initialize(maingame = new MainGame(new AndroidData(this)), true);
 	}
 	
 	@Override
