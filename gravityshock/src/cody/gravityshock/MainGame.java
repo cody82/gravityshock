47a770a8ef71ad56f32784a424e6bc6de6598545
cody <cody@localhost>
2012-03-30 22:21:30 +0200

    add bloom

--- gravityshock/src/cody/gravityshock/MainGame.java
+++ gravityshock/src/cody/gravityshock/MainGame.java
@@ -1,26 +1,36 @@
 package cody.gravityshock;
 
+import bloom.Bloom;
+
 import com.badlogic.gdx.Game;
 
 public class MainGame extends Game{
 	public int level = 1;
 	public int numplayers = 1;
+	public boolean enable_bloom = true;
+	Bloom bloom;
 	
 	public MainGame() {
 	}
 	@Override
 	public void create() {
+		if(enable_bloom)
+			bloom = new Bloom();
 		this.setScreen(new MainMenu(this));
 	}
 
 	@Override
 	public void render() {
-		
+		boolean b = bloom != null;
+		if(b)
+			bloom.capture();
 		super.render();
 		if(gotoMainMenu) {
 			gotoMainMenu = false;
 			startMainMenu();
 		}
+		if(b)
+			bloom.render();
 	}
 	public void start() {
 		Main main = new Main(this);
